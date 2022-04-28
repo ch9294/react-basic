e6eea809aae8e17d85a923fed03c82d058a9e4cd
최천일 <ch9294@choecheon-il-ui-MacBookPro.local>
2022-04-28 23:30:37 +0900

    2022-04-28 웹팩 개발서버 및 핫 리로딩 설정

--- word-game/WordRelay.jsx
+++ word-game/WordRelay.jsx
@@ -1,10 +1,55 @@
 const React = require('react');
 
-const WordRelay = () => {
+class WordRelay extends React.Component {
+    state = {
+        word: "제로초", value: "", result: ""
+    }
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        if (this.state.word[this.state.word.length - 1] === e.target[0].value[0]) {
+            this.setState(prev => {
+                this.input.focus();
+                return {
+                    word: e.target[0].value,
+                    value: '',
+                    result: "정답입니다."
+                }
+            });
+        } else {
+            this.setState(prev => {
+                this.input.focus();
+                return {
+                    ...prev, result: "정답이 아닙니다.", value: ""
+                }
+            });
+        }
+    }
 
-    return (<div>
-        끝말잇기
-    </div>)
+    onChange = (e) => {
+        console.log(e.target.value);
+        this.setState({
+            value: e.target.value
+        });
+    }
+
+    input;
+    onRefInput = (c) => {
+        this.input = c;
+    }
+
+    render() {
+        return (
+            <>
+                <div>{this.state.word}</div>
+                <form onSubmit={this.onSubmitForm}>
+                    <input ref={this.onRefInput} type="text" value={this.state.value} onChange={this.onChange}/>
+                    <button>입력</button>
+                </form>
+                <div>{this.state.result}</div>
+            </>
+        );
+    }
+
 }
 
 
--- word-game/webpack.config.js
+++ word-game/webpack.config.js
@@ -1,4 +1,6 @@
-const path = require('path')
+const path = require('path');
+const reactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     name: 'word-relay-setting',
@@ -11,18 +13,41 @@ module.exports = {
         app: ['./client'],
     },
     module: {
-        rules:[
+        rules: [
             {
                 test: /\.jsx>?/,
                 loader: 'babel-loader',
                 options: {
-                    presets:['@babel/preset-env','@babel/preset-react']
+                    presets: [
+                        ['@babel/preset-env', {
+                            targets: {
+                                browsers: ['> 1% in KR']
+                            },
+                            debug: true
+                        }]
+                        , '@babel/preset-react'
+                    ],
+                    plugins: [
+                        // '@babel/plugin-proposal-class-properties',
+                        'react-refresh/babel'
+                    ]
                 }
             }
         ]
     },
+    plugins: [
+        new reactRefreshPlugin(),
+        new HtmlWebpackPlugin({
+            template:"./word-game.html",
+            filename:"./index.html"
+        })
+    ],
     output: { // 출력
         path: path.join(__dirname, 'dist'),
-        filename: 'app.js'
+        filename: 'app.js',
+    },
+    devServer: { // 개발서버 설정
+        // publicPath: '/dist/',
+        hot: true
     }
 }
